0a7258ae6d45ad6f47fda82f1e0b4e31eb17b89d
Nickolay <nzaharov86@gmail.com>
2018-09-28 16:17:33 +0300

    Change absolute uri

--- client/main.js
+++ client/main.js
@@ -6,6 +6,6 @@ import '../imports/startup/accounts-config.js';
 import App from '../imports/ui/App';
  
 Meteor.startup(() => {
-  Meteor.absoluteUrl.defaultOptions.rootUrl = 'meteor-test-deploy.herokuapp.com/_oauth/google';
+  Meteor.absoluteUrl.defaultOptions.rootUrl = 'meteor-test-deploy.herokuapp.com';
   render(<App />, document.getElementById('render-target'));
 });
