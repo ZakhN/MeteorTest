1fb6c432da92c86712ee5a8169a9e2060093e07d
Nickolay <nzaharov86@gmail.com>
2018-09-28 13:28:14 +0300

    Add meteor login google

--- imports/ui/AccountsUIWrapper.js
+++ imports/ui/AccountsUIWrapper.js
@@ -8,20 +8,15 @@ export default class AccountsUIWrapper extends Component {
     // Use Meteor Blaze to render login buttons
     this.view = Blaze.render(Template.loginButtons,
       ReactDOM.findDOMNode(this.refs.container));
-<<<<<<< HEAD
-      
-/*      Meteor.loginWithGoogle({
+      Meteor.loginWithGoogle({
         requestPermissions: ['email']
       }, function(error) {
         if (error) {
           console.log(error); //If there is any error, will get error here
         }else{
           console.log(Meteor.user());// If there is successful login, you will get login details here
-          }
+        }
       });
-*/
-=======
->>>>>>> parent of 4f1a297... Add google auth
   }
   componentWillUnmount() {
     // Clean up Blaze view
