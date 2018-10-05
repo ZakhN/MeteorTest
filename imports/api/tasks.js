a089958693b225b3f2ac4846bb81d43c427171e0
Nickolay <nzaharov86@gmail.com>
2018-10-05 15:46:35 +0300

    Add limiter

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -2,14 +2,18 @@ import { Mongo } from 'meteor/mongo';
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 import { ValidatedMethod } from 'meteor/mdg:validated-method';
+import { DDPRateLimiter } from 'meteor/ddp-rate-limiter'
+import _ from 'lodash';
 import SimpleSchema from 'simpl-schema';
 
 export const Tasks = new Mongo.Collection('tasks');
 
+// DDPRateLimiter.addRule(matcher, numRequests, timeInterval);
+
 /*  tasks.remove
     remove events from db and gCalengar
 */
-new ValidatedMethod({
+const taskRemove = new ValidatedMethod({
   name: 'tasks.remove',
   validate: new SimpleSchema({
     taskId: { type: String, regEx: SimpleSchema.RegEx.Id },
@@ -39,7 +43,7 @@ new ValidatedMethod({
 /*  tasks.hideChecked
     hiden checked events from ui
 */
-new ValidatedMethod({
+const taskHideCheked = new ValidatedMethod({
   name: 'tasks.hideChecked',
   validate: new SimpleSchema({
     isCheked: { type: Boolean },
@@ -52,7 +56,7 @@ new ValidatedMethod({
 /*  tasks.setPrivate
     set events to private
 */
-new ValidatedMethod({
+const taskSetPrivate = new ValidatedMethod({
   name: 'tasks.setPrivate',
   validate: new SimpleSchema({
     taskId: { type: String, regEx: SimpleSchema.RegEx.Id },
@@ -71,7 +75,7 @@ new ValidatedMethod({
 /*  tasks.setChecked
     set events to checked
 */
-new ValidatedMethod({
+const taskSetChecked = new ValidatedMethod({
   name: 'tasks.setChecked',
   validate: new SimpleSchema({
     setChecked: { type: Boolean },
@@ -85,7 +89,7 @@ new ValidatedMethod({
 /*  tasks.insert
     insert events to the collection
 */
-new ValidatedMethod({
+const taskInsert = new ValidatedMethod({
   name: 'tasks.insert',
   validate: new SimpleSchema({
     text: { type: String },
@@ -154,6 +158,7 @@ new ValidatedMethod({
 
 
 
+
 if (Meteor.isServer) {
   Meteor.publish('tasks', function tasksPublication() {
     return Tasks.find({
@@ -163,67 +168,21 @@ if (Meteor.isServer) {
       ],
     });
   });
+
+  const LISTS_METHODS = _.map([
+    taskRemove,
+    taskHideCheked,
+    taskSetPrivate,
+    taskSetChecked,
+    taskInsert,
+  ], 'name');
+
+  DDPRateLimiter.addRule({
+    name(name) {
+      return _.includes(LISTS_METHODS, name);
+    },
+
+    // Rate limit per connection ID
+    connectionId() { return true; }
+  }, 5, 1000);
 }
-
-Meteor.methods({
-  // 'tasks.insert': async function(text, sendToCalendar){
-  //   check(text, String);
-  //   check(sendToCalendar, Boolean);
-    
-  //   if (!this.userId) {
-  //     throw new Meteor.Error('not-authorized');
-  //   }
-    
-  //   let codePhrase = '';
-  //   let codePhraseTime = '';
-  //   //TODO Разделить текст с кодовой фразой и передавать в Tasks чистый текст
-  //   const todayReg = /(^| )сегодня(\W|$)/gi;
-  //   const tomorrowReg = /(^| )завтра(\W|$)/gi;
-  //   const datReg = /((^| )сегодня(\W|$)|(^| )завтра(\W|$))/ig;
-  //   const timeReg = /(?:[1-9]|1[0-2]):[0-9]{2}\s(?:AM|PM)/ig;
-    
-  //   if (((text.match(timeReg)) && (!text.match(datReg)))) throw new Meteor.Error('There is no date','Time determined, date not');
-
-  //   const date = new Date();
-    
-  //   if (text.match(todayReg)) codePhrase = new Date().toLocaleDateString();
-
-  //   else if (text.match(tomorrowReg)) codePhrase = new Date(date.setDate(date.getDate() + 1)).toLocaleDateString();
-
-  //   if (text.match(timeReg)) codePhraseTime = text.match(timeReg);
-
-  //   codePhrase = codePhrase + ' ' + codePhraseTime;
-
-  //   const task = {
-  //     text,
-  //     createdAt: new Date(),
-  //     owner: this.userId,
-  //     username: Meteor.user().username,
-  //   };
-    
-  //   if (codePhrase.length > 1) task.dueDate = new Date(moment.utc(codePhrase).format());
-
-  //   if (Meteor.isServer && sendToCalendar) {
-  //     import * as GoogleCalendar from '../integrations/google/calendar';
-
-  //     let event = {
-  //       summary: 'meteor-event',
-  //       description: text,
-  //       start: {
-  //         dateTime: moment(codePhrase).format(),
-  //       },
-  //       end: {
-  //         dateTime: moment(codePhrase).format(),
-  //       },
-  //     };
-
-  //     const createdEvent = await GoogleCalendar.createEvent({
-  //       event,
-  //       userId: Meteor.userId(),
-  //     });
-      
-  //     task.calendarEventId = createdEvent.id;
-  //   }
-  //   Tasks.insert(task);
-  // },
-});
